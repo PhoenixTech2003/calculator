198fb6a3d06a84947ed9bc68062af84a22c57b4a
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2023-08-24 20:40:03 +0200

    Create SubtractValues function in code.js

--- js/code.js
+++ js/code.js
@@ -2,3 +2,9 @@ function addValues (firstValue,...values){
     return firstValue + values.reduce((total,value)=> total += value,0);
 };
 
+function subtractValues (firstValue,...values){
+
+    return  values.reduce((total,value)=> total -= value,firstValue);
+};
+
+subtractValues(5,2)
